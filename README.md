1eab59c42e929925d1329d402507064740ae2b73
3manuel0 <saiddimension@gmail.com>
2026-05-29 15:43:37 +0100

    Header

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Goal is to parce bitmap foramt (.bmp).
 
-[bmp details](https://users.cs.fiu.edu/~czhang/teaching/cop4225/project_files/bitmap_format.htm)
+#### Sites
+
+[site](https://users.cs.fiu.edu/~czhang/teaching/cop4225/project_files/bitmap_format.htm)
+[wikipedia](https://en.wikipedia.org/wiki/BMP_file_format)
--- bmp.c
+++ bmp.c
@@ -6,7 +6,25 @@ typedef struct{
     u32 size;
     u32 res;
     u32 offset;
+    i32 width;
+    i32 height;
+    u32 compression;
 } bmpheader;
+
+bmpheader bmp_get_header(Buffer buffer){
+    bmpheader header = {0};
+
+
+    header.seg = *(u16 *)buffer.buf;
+    header.size = *(u32 *)(buffer.buf + 2);
+    header.res = *(u32 *)(buffer.buf + 6);
+    header.offset = *(u32 *)(buffer.buf + 10);
+    header.width = *(i32 *)(buffer.buf + 18);
+    header.height = *(i32 *)(buffer.buf + 22);
+    header.compression = *(i32 *)(buffer.buf + 30);
+
+    return header;
+}
 
 int main(){
     
@@ -14,14 +32,8 @@ int main(){
 
 
     // printf("\n%u", *(unsigned int *)buffer.buf);
-    bmpheader header = {};
-    header.seg = *(u16 *)buffer.buf;
-    header.size = *(u32 *)(buffer.buf + 2);
-    header.res = *(u32 *)(buffer.buf + 6);
-    header.offset = *(u32 *)(buffer.buf + 10);
-    i32 width = *(i32 *)(buffer.buf + 18);
-    i32 height = *(i32 *)(buffer.buf + 22);
+    bmpheader header = bmp_get_header(buffer);
     printf("%.2s, %u, %u \n", (char *)&header.seg, header.size, header.offset);
-    printf("%d %d \n", width, height);
+    printf("%d %d %x\n", header.width, header.height, header.compression);
 
 }
